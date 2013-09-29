ff37ce85ecd4465d79329cc074a9a045b9cf30f1
Trevor Hodde <trhodde@wpi.edu>
2013-09-29 17:47:28 -0400

    Adding tasks for Trevor

---
+++ src/TrevorTasks.java
@@ -0,0 +1,20 @@
+/**
+ * User: trevor hodde
+ * Date: 9/29/13
+ * Time: 2:44 PM
+ */
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class TrevorTasks {
+    public static void doTaskB() {
+
+    }
+}
